26bb4932018c95d1ecf941cf15454c724f4c5d5d
Amagi_Yukisaki <xingzeyu2001@gmail.com>
2020-07-22 11:32:13 +0800

    implementing chord

--- src/chord/chord.go
+++ src/chord/chord.go
@@ -1,6 +1,7 @@
 package chord
 
 import (
+	"errors"
 	"fmt"
 	"math/big"
 	"sync"
@@ -13,23 +14,100 @@ const (
 )
 
 type Edge struct {
-	tar string
+	ip  string
 	val big.Int
 }
 
 type Storage struct {
-	Map  map[string]string
+	data map[string]string
 	lock sync.Mutex // for multithreading
 }
 
 type Node struct {
-	es        [Len]Edge
-	pre       Edge
-	suc       Edge
-	data_lock sync.Mutex
+	finger [Len]Edge
+	pre    Edge
+	suc    Edge
+	lock   sync.Mutex
 
 	sto    Storage
 	stoPre Storage
 
+	pos   big.Int
+	ip    string
 	state bool // 1 for on
+}
+
+func (pos *Node) queryInside(tar string) string {
+	var value string
+	var ok bool
+
+	pos.sto.lock.Lock()
+	value, ok = pos.sto.data[tar]
+	pos.sto.lock.Unlock()
+
+	if ok {
+		return value
+	} else {
+		fmt.Print("Error(0):: Value Not Found.")
+		return ""
+	}
+}
+
+func (pos *Node) findNearestSuc(val *big.Int) Edge {
+	for i := Len - 1; i > 0; i-- {
+		temp := new(big.Int)
+		if Dis(val, &pos.pos).Cmp(Dis(val, temp.Add(&pos.pos, powTwo(int64(i))))) < 0 {
+			pos.lock.Lock()
+			var ret = pos.finger[i]
+			pos.lock.Unlock()
+			return ret
+		}
+	}
+	return Edge{pos.ip, pos.pos}
+}
+
+// return node ip for a query key.
+func (pos *Node) findNode(ret *string, tar string) error {
+	nxt := pos.findNearestSuc(hashStr(tar))
+	if nxt.ip == pos.ip {
+		*ret = pos.ip
+		return nil
+	} else {
+		client := Dial(nxt.ip)
+		if client == nil {
+			fmt.Print("Error(1):: Dial Connect Failure.")
+			*ret = ""
+			return errors.New("Error(1):: Dial Connect Failure.")
+		} else {
+			err := client.Call("Node.findNode", nxt, tar)
+			if err != nil {
+				fmt.Print("Error(2):: RPC Calling Failure.")
+				*ret = ""
+				return errors.New("Error(2):: RPC Calling Failure.")
+			}
+		}
+	}
+	return nil
+}
+
+func (pos *Node) queryVal(ret *string, tar string) error {
+	var ip string
+	err := pos.findNode(&ip, tar)
+	if err != nil {
+		fmt.Print("Error(3):: Query Failure.")
+		return errors.New("Error(0):: Value Not Found.")
+	}
+	client := Dial(ip)
+	if client == nil {
+		fmt.Print("Error(1):: Dial Connect Failure.")
+		*ret = ""
+		return errors.New("Error(1):: Dial Connect Failure.")
+	}
+	err = client.Call("Node.queryInside", ret, tar)
+	if err != nil {
+		fmt.Print("Error(2):: RPC Calling Failure.")
+		*ret = ""
+		return errors.New("Error(2):: RPC Calling Failure.")
+	}
+	return nil
 }
--- src/chord/utilities.go
+++ src/chord/utilities.go
@@ -11,6 +11,11 @@ const (
 	tryTime = 5
 )
 
+var (
+	two = big.NewInt(2)
+	mod = new(big.Int).Exp(two, big.NewInt(160), nil)
+)
+
 func hashStr(s string) *big.Int {
 	h := sha1.New()
 	h.Write([]byte(s))
@@ -29,4 +34,13 @@ func Dial(s string) *rpc.Client {
 		}
 	}
 	return nil
+}
+
+func powTwo(p int64) *big.Int {
+	return new(big.Int).Exp(two, big.NewInt(p), nil)
+}
+
+func Dis(tar *big.Int, pos *big.Int) *big.Int {
+	temp := new(big.Int)
+	return temp.Mod(temp.Add(temp.Sub(tar, pos), mod), mod)
 }
